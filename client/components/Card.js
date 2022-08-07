f989a1842a7b1e281e60429118ac75d01fe60319
Frank <frankabasham@gmail.com>
2022-08-06 21:37:45 -0400

    change styles

--- client/components/Card.js
+++ client/components/Card.js
@@ -5,19 +5,19 @@ import {BiUpvote,BiDownvote} from 'react-icons/bi'
 export default function Card({props}){
   let {url,createdTime,title,score,ups,downs,permalink} = props
   return (
-    <div className='flex flex-col justify-center items-center bg-slate-700 rounded-xl p-2 pt-2 pb-4 text-white shadow-2xl border-4 border-slate-800'>
-      <p className='text-xl text-center mb-2'>{title}</p>
-      <div className='flex items-center gap-6 mb-2'>
-        <span className='flex items-center text-green-400 gap-1'>
+    <div className='flex flex-col items-center justify-center p-2 pt-2 pb-4 text-white border-2 shadow-2xl bg-slate-700 rounded-xl border-slate-800'>
+      <p className='mb-2 text-xl font-semibold text-center'>{title}</p>
+      <div className='flex items-center gap-6 mb-4'>
+        <span className='flex items-center gap-1 text-green-400'>
           <BiUpvote />
           <p>{ups}</p>
         </span>
-        <span className='flex items-center text-red-400 gap-1'>
+        <span className='flex items-center gap-1 text-red-400'>
           <BiDownvote />
           <p>{downs}</p>
         </span>
       </div>
-      <div className='relative h-96 w-64 border-2 border-slate-800 rounded-xl hover:border-white focus-within:border-white'>
+      <div className='relative w-64 mb-2 border-2 h-96 border-slate-800 rounded-xl hover:border-white focus-within:border-white'>
         <a href={'https://old.reddit.com'+permalink} className='' target='_blank'>
           <Image src={url} objectFit='cover' layout='fill' className='rounded-xl'/>
         </a>
